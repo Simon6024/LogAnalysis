eb8e3ee972b4f2c2f0e0b3acbf1aec32dbfc333c
rich <rich.grandmaison@gmail.com>
2017-04-15 11:57:57 -0400

    started work on code to read args and read the logs

--- src/log_analyzer.java
+++ src/log_analyzer.java
@@ -1,14 +1,46 @@
+import java.io.File;
+
 
 public class log_analyzer 
 {
+	public static String LOGS = "./logs/";
+	public static int question;
 
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
-
+		try{
+			if(!args[0].equals("-q")){
+				System.out.println("Invalid argument. Expected -q...");
+			}
+			question = Integer.parseInt(args[1]);
+		} catch(Exception e){
+			System.out.println("Insufficient arguments!");
+		}
+		if(args.length < 3){
+			System.out.println("Insufficient arguments!");
+		}		
+		readFileFromArgs(args);
+		
+	}	
+	
+	public static void readFileFromArgs(String [] args){
+		File source;
+		for(int i = 2; i < args.length;i++){
+			source = new File(LOGS + args[i]);
+			if(source.exists() && source.isDirectory()){
+				readFilesForFolder(source);
+			}
+		}
+	}	
+	public static void readFilesForFolder(final File source) {
+	    for (final File fileEntry : source.listFiles()) {
+	        if (fileEntry.isDirectory()) {
+	        	readFilesForFolder(fileEntry);
+	        } else {
+	            System.out.println(fileEntry.getName());
+	        }
+	    }
 	}
 
 }
